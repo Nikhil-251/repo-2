e0cfbad548d86219c8d7b9da48a6f170b451bac6
Nikhil Gupta <77400458+Nikhil-251@users.noreply.github.com>
2024-09-03 12:33:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Welcome to Python Flask World V2.0 with CI/CD'
+    return 'Welcome to Python Flask World V1.0 with CI/CD Deployed on the APP Engine'
 
 
 if __name__ == '__main__':
